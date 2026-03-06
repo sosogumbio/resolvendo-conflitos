32a8dcf6689a42ee67c14f1dc7622af9df4f6e4e
SOPHIA ISABELLE RODRIGUES GUMBIO <sophia.gumbio@portalsesisp.org.br>
2026-03-06 11:26:18 -0300

    alunoA inserindo linha 3, 4 e 5

--- conflito.ts
+++ conflito.ts
@@ -1,2 +1,5 @@
 console.log(`alunoA alterou a linha 1 do arquivo conflito.ts`);
 console.log(`alunoB inserindo a linha 2`);
+
+let concatenar: string = `Git` + `Hub`;
+console.log(`Concatenado ${concatenar}`);
